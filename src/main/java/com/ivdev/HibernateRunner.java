202a5ef5c54f3b8360e35cb632af23db5c62ae9b
Ivan Rebrischev <ivbl21@ya.ru>
2023-04-25 15:41:29 +0300

    HibernateRunner created

--- src/main/java/com/ivdev/HibernateRunner.java
+++ src/main/java/com/ivdev/HibernateRunner.java
@@ -1,12 +1,15 @@
 package com.ivdev;
 
+import com.ivdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
@@ -25,16 +28,29 @@ public class HibernateRunner {
 
         //*********обертки-аналоги для connection/pool через hibernate, на основе cfg.xml********
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         //возвращает фабрику на основании всех полей класса Configuration + cfg.xml
         //один экземпляр на все приложение, как и connection pool
         try(SessionFactory sf = configuration.buildSessionFactory();
+
             //обертка для connection на уровне hibernate для управления сущностями
             Session ss = sf.openSession()) {
-            System.out.println("OK");
+            ss.beginTransaction();
+
+            //создадим сущность и сохраним в БД
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ryaba")
+                    .birthDate(LocalDate.of(2020, 1, 15))
+                    .age(19)
+                    .build();
+            ss.persist(user);
+
+            ss.getTransaction().commit();
         }
-
-
     }
 }
