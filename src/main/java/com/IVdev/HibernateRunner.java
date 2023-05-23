13d133c1bf5ea1e6a2efebc8beae73168a1ce17c
Ivan Rebrischev <ivbl21@ya.ru>
2023-05-23 17:55:17 +0300

    refresh, merge, persist added

--- src/main/java/com/IVdev/HibernateRunner.java
+++ src/main/java/com/IVdev/HibernateRunner.java
@@ -1,87 +1,66 @@
 package com.IVdev;
 
-import com.IVdev.converter.BirthdayConverter;
-import com.IVdev.entity.Birthday;
-import com.IVdev.entity.Role;
 import com.IVdev.entity.User;
-import com.IVdev.type.JsonType;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.IVdev.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
 
-        //**********подключение к БД через Driver Manager************
-
-//        создание пула подключений
-//        BlockingDeque<Connection> pool = null;
-//        Connection connection = pool.take();
-
-//        коннект к БД через параметры
-//        Connection connection = DriverManager
-//                .getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "pass2023");
-
-
-        //*********обертки-аналоги для connection/pool через hibernate, на основе cfg.xml********
-        Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
-
-        //автоустановка кастомного конвертера
-        //hibernate автоматически применит конвертер ко всем полям Birthday в сущностях
-        configuration.addAttributeConverter(BirthdayConverter.class, true);
-        //configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
+        //entyty is transient
+        User user = User.builder()
+                .username("ivan@gmail.com")
+                .lastname("Ivanov")
+                .firstname("Ivan")
+                .build();
 
         //возвращает фабрику на основании всех полей класса Configuration + cfg.xml
         //один экземпляр на все приложение, как и connection pool
-        try(SessionFactory sf = configuration.buildSessionFactory();
+        try(SessionFactory sf = HibernateUtil.buildSessionFactory()) {
+            try(Session session1 = sf.openSession()) {
+                session1.beginTransaction();
 
-            //обертка для connection на уровне hibernate для управления сущностями
-            Session ss = sf.openSession()) {
-            ss.beginTransaction();
+                //entity is in persistent state
+                session1.persist(user);
 
-            //создадим сущность и сохраним в БД
-//            User user = User.builder()
-//                    .username("ivan@ya.ru")
-//                    .birthDate(new Birthday(LocalDate.of(2020, 1, 15)))
-//                    .firstname("IV")
-//                    .lastname("B.")
-//                    .role(Role.ADMIN)
-//                    .info("""
-//                            {
-//                                "name": "Ivan",
-//                                "id": 25
-//                            }
-//                            """)
-//                    .build();
-            //persist используется для save-опции с версии 6.0
-//            ss.persist(user);
-//            ss.update(user);
-            //используется remove() с версии 6.0
-//            ss.delete(user);
-            //используется merge  с версии 6.0
-//            ss.saveOrUpdate(user);
+                session1.getTransaction().commit();
+            }
+            try(Session session2 = sf.openSession()) {
+                session2.beginTransaction();
 
-            /*
-            SessionFactory: Enitites + MetaModel + BasicTypes.
+                //works like: transient -> persistent(through select-method) -> removed
+//                session2.remove(user);
 
-            get работает через byId, что возвращает сущность по ее идентификатору
-            id - ключевой момент работы hibernate
-            Сущность тесно связана с классом EntityPersister, что получается из метамодели фабрики сессий.
-            EntityPersister преобразует объектную модель - в реляционную и обратно, ответственнен за CRUD-операции над Entity
-            Количество EntityPersister == количеству сущностей.
-            В метамодели хранится инфо о всех сущностях (EntityPersisterMap) и типах в приложении, в т.ч. и basic
-            После byId в цепочке вызывается метод load (основан на listeners), что знает, как загружать соответствующую сущность.
-            */
 
-            User user = ss.get(User.class, "\"ivan@ya.ru\"");
-            ss.getTransaction().commit();
+                /*
+                refresh getting actual info from DB and mapping all fields onto entity
+                Literally next:
+
+                User freshUser = get(User.class, user.getUsername());
+                user.setLastname(freshUser.getLastname);
+                user.setFirstname(freshUser.getFirstname);
+
+                user.setFirstname("Oleg");
+                session2.refresh(user);
+                */
+
+
+                /*
+                merge working contrary: request to DB -> create new user -> set fields from old user
+                Literally next:
+
+                User freshUser = get(User.class, user.getUsername());
+                freshUser.setLastname(user.getLastname);
+                freshUser.setFirstname(user.getFirstname);
+
+                Object mergedUser = session2.merge(user);
+                */
+
+                session2.getTransaction().commit();
+            }
         }
     }
 }
---
+++ src/main/java/com/IVdev/util/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.IVdev.util;
+
+import com.IVdev.converter.BirthdayConverter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//вынесем функционал для сессий из HibernateRunner в отдельный класс
+//для удобства тестирования + чистоты кода
+public class HibernateUtil {
+    public static SessionFactory buildSessionFactory() {
+
+        Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
+
+        //автоустановка кастомного конвертера
+        //hibernate автоматически применит конвертер ко всем полям Birthday в сущностях
+        configuration.addAttributeConverter(BirthdayConverter.class, true);
+
+//        configuration.registerTypeOverride(new JsonBinaryType());
+        configuration.configure();
+        return configuration.buildSessionFactory();
+    }
+}
